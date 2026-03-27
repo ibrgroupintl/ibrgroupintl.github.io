9b9170aec8535ea20a2b4a51ff62af90ae01fa4e
Alfred Anate Bodurin MAYAKI <alfred.mayaki@ibrecruitment.com>
2026-03-27 19:06:27 +0000

    Firebase deployment credentials

---
+++ app/public/firebase-config.js
@@ -0,0 +1,9 @@
+// public/firebase-config.js
+// replace the placeholders with values from Firebase Console
+window.firebaseConfig = {
+  apiKey: "AIzaSyCXPA4TasdF-pX7Ey5Tyf_aSrufZzCqPxo",
+  authDomain: "supa-73a39.web.app",
+  projectId: "supa-73a39",
+  appId: "1:447556072560:web:4f2831fd7f86e23cf470b5",
+  // other fields...
+};
