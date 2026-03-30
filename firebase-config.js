068a270cda1bf67d46bb98cbf1c7dea50d44a53a
Alfred Anate Bodurin MAYAKI <alfred.mayaki@ibrecruitment.com>
2026-03-30 08:44:40 +0100

    Minor Firebase config changes

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // public/firebase-config.js
 // replace the placeholders with values from Firebase Console
 window.firebaseConfig = {
-  apiKey: "AIzaSyCXPA4TasdF-pX7Ey5Tyf_aSrufZzCqPxo",
-  authDomain: "supa-73a39.web.app",
-  projectId: "supa-73a39",
-  appId: "1:447556072560:web:4f2831fd7f86e23cf470b5",
+  apiKey: "AIzaSyCcsm1Pvh_h1M1OySEXTYeZnOtBUAYExio",
+  authDomain: "wale-491803.firebaseapp.com",
+  projectId: "wale-491803",
+  appId: "1:916707769747:web:dcc1f83b793c101e7d7c66",
   // other fields...
 };
